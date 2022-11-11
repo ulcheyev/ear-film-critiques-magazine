c9fa3bf7694d08c3c2bf36fdfc1c28a732a65e17
Lupenko, Margarita <lupenmar@fel.cvut.cz>
2022-11-11 18:12:26 +0100

    Add parts of documentation and some service

---
+++ CP1/src/main/java/cvut/config/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package cvut.config.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String str) {
+        super(str);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+}
--- CP1/src/main/java/cvut/model/Comment.java
+++ CP1/src/main/java/cvut/model/Comment.java
@@ -37,6 +37,9 @@ public class Comment{
         this.appUser = appUser;
     }
 
+    public Comment(String text, Date date, AppUser appUser, Critique critique) {
+    }
+
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
 
