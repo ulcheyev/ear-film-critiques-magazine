04ed8a3cb71c0a5ae0afb941f35f393772cd77c9
Lupenko, Margarita <lupenmar@fel.cvut.cz>
2022-12-04 13:34:49 +0100

    Add NamedQuery AppUser, Comment, Critique

--- CP2/src/main/java/cvut/repository/AppUserRepository.java
+++ CP2/src/main/java/cvut/repository/AppUserRepository.java
@@ -3,10 +3,19 @@ import cvut.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@NamedQueries({
+        @NamedQuery(name = "AppUserRepository.findByUsername", query = "SELECT appU FROM AppUser appU WHERE appU.username = :username"),
+        @NamedQuery(name = "AppUserRepository.findAppUserByEmail", query = "SELECT appU FROM AppUser appU WHERE appU.email = :email"),
+        @NamedQuery(name = "AppUserRepository.findAppUsersByLastname", query = "SELECT appU FROM AppUser appU WHERE appU.lastname = :lastname"),
+        @NamedQuery(name = "AppUserRepository.findAppUsersByFirstname", query = "SELECT appU FROM AppUser appU WHERE appU.firstname = :firstname"),
+        @NamedQuery(name = "AppUserRepository.findAppUsersByFirstnameAndLastname", query = "SELECT appU FROM AppUser appU WHERE appU.firstname = :firstname AND appU.lastname = :lastname")
+})
 public interface AppUserRepository extends JpaRepository<AppUser, Long>{
 
     Optional<AppUser> findAppUserByUsername(String username);
--- CP2/src/main/java/cvut/repository/CommentRepository.java
+++ CP2/src/main/java/cvut/repository/CommentRepository.java
@@ -6,11 +6,21 @@ import cvut.model.Critic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@NamedQueries({
+        @NamedQuery(name = "CommentRepository.findByCritiqueId", query = "SELECT comm FROM Comment comm WHERE comm.id = ?1 AND comm.critique.critiqueState = cvut.model.CritiqueState.ACCEPTED"),
+        @NamedQuery(name = "CommentRepository.findByCritiqueId", query = "SELECT comm FROM Comment comm WHERE comm.critique.id = ?1 AND comm.critique.critiqueState = cvut.model.CritiqueState.ACCEPTED"),
+        @NamedQuery(name = "CommentRepository.findAllByAppUser_Id", query = "SELECT comm FROM Comment comm WHERE comm.appUser.id = ?1 AND comm.critique.critiqueState = cvut.model.CritiqueState.ACCEPTED"),
+        @NamedQuery(name = "CommentRepository.findAllByDateOfPublic", query = "SELECT comm FROM Comment comm WHERE comm.dateOfPublic = ?1 AND comm.critique.critiqueState = cvut.model.CritiqueState.ACCEPTED"),
+        @NamedQuery(name = "CommentRepository.findAllByCritique_IdOrderByDateOfPublicDesc", query = "SELECT comm FROM Comment comm WHERE comm.dateOfPublic = ?1 AND comm.critique.critiqueState = cvut.model.CritiqueState.ACCEPTED ORDER BY comm.dateOfPublic DESC"),
+        @NamedQuery(name = "CommentRepository.findAllByCritique_IdOrderByDateOfPublicAs", query = "SELECT comm FROM Comment comm WHERE comm.dateOfPublic = ?1 AND comm.critique.critiqueState = cvut.model.CritiqueState.ACCEPTED ORDER BY comm.dateOfPublic ASC")
+})
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByAppUser_Id(Long id);
